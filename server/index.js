eb98bc55b36c7e335f3c7f2686ac7e449e1a29fa
Joe Redmon <joesredmon@gmail.com>
2020-12-03 10:41:52 -0800

    basic server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require(express);
+const app = express();
+const bodyParser = require(body-parser);
+const path = require(path);
+const axios = require(axios);
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/../client/dist')));
+
+const PORT = 3002;
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`)
+});
