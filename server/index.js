2bb64a75bcd46af8285ee208c930175e14d42de0
Joe Redmon <joesredmon@gmail.com>
2020-12-08 11:30:15 -0800

    api calls working to quickbase

--- server/index.js
+++ server/index.js
@@ -3,14 +3,21 @@ const app = express();
 const bodyParser = require("body-parser");
 const path = require("path");
 const axios = require("axios");
+const token = require('../quickbase.js');
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 const PORT = 3002;
 
+const morseSteelTeamMembers = `https://morsesteel.quickbase.com/db/bqv7vttbx`;
+
 app.get('/api/morse/clients', (req, res) => {
-  res.send('get');
+  const reqUrl = `${morseSteelTeamMembers}?a=API_DoQuery&includeRids=1&query={10.EX.'Foreman'}&usertoken=${token}`;
+
+  axios(reqUrl)
+    .then(({ data }) =>res.send(data))
+    .catch((err) => console.log(err));
 });
 
 app.listen(PORT, () => {
