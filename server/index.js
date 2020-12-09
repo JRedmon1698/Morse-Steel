b0ca99648c5283b6868c527f922be98884db20e4
Joe Redmon <joesredmon@gmail.com>
2020-12-09 11:15:34 -0800

    adding api for jobs

--- server/index.js
+++ server/index.js
@@ -10,13 +10,24 @@ app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 const PORT = 3002;
 
-const morseSteelTeamMembers = `https://morsesteel.quickbase.com/db/bqv7vttbx`;
+const morseSteelTeamMembers = 'https://morsesteel.quickbase.com/db/bqv7vttbx';
+const morseSteelJobs = 'https://morsesteel.quickbase.com/db/bqs3mx37c';
 
+// team members
 app.get('/api/morse/clients', (req, res) => {
   const reqUrl = `${morseSteelTeamMembers}?a=API_DoQuery&includeRids=1&query={10.EX.'Foreman'}&usertoken=${token}`;
 
   axios(reqUrl)
     .then(({ data }) =>res.send(data))
+    .catch((err) => console.log(err));
+});
+
+// jobs
+app.get('/api/morse/jobs', (req, res) => {
+  const reqUrl = `${morseSteelJobs}?a=API_DoQuery&usertoken=${token}`;
+
+  axios(reqUrl)
+    .then(({ data }) => res.send(data))
     .catch((err) => console.log(err));
 });
 
