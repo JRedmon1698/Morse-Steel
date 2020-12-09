6b4f9a68f97d952586b900d1649962b4def87d68
Joe Redmon <joesredmon@gmail.com>
2020-12-09 11:25:04 -0800

    working on front end display of jobs and team members with working api calls

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Jobs from './Jobs.jsx';
+import TeamMembers from './TeamMembers.jsx';
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  const getJobs = () => {
+    axios.get('/api/morse/jobs')
+      .then((data) => {
+        setJobs(data);
+      })
+      .catch((err) => console.log(err));
+  };
+
     return (
         <div>React is working</div>
     );
