99ea88cf3779038524696471d3ebbd1dfb649475
Joe Redmon <joesredmon@gmail.com>
2020-12-13 18:06:21 -0800

    working team member api json call

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -65,7 +65,6 @@ function App() {
       </div>
     );
   };
-
 };
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const appId = 'bqs3mx358';
 const projectTableId = 'bqs3mx37c';
 const teamMemberTableId = 'bqv7vttbx';
 
-// team members
+// team members, non-json
 app.get('/api/morse/team', (req, res) => {
   const reqUrl = `${morseSteelTeamMembers}?a=API_DoQuery&fmt=structured&includeRids=1&query={10.EX.'Foreman'}&usertoken=${token}`;
 
@@ -27,7 +27,48 @@ app.get('/api/morse/team', (req, res) => {
     .catch((err) => console.log(err));
 });
 
-// projects
+// team members, json
+app.get('/api/morse/json/team', (req, res) => {
+  const reqUrl = `https://api.quickbase.com/v1/records/query`;
+
+  const headers = {
+    'QB-Realm-Hostname': `${hostName}`,
+    'User-Agent': 'Joe',
+    'Authorization': `QB-USER-TOKEN ${token}`,
+    'Content-Type': 'application/json'
+  };
+
+  const body = {
+    'from': `${teamMemberTableId}`,
+    'select': [
+      35, 7
+    ],
+    'where': '',
+    'sortBy': false,
+    'groupBy': [
+      {
+        'fieldId': 7,
+        'grouping': 'ASC'
+      },
+    ],
+    'options': {
+      'skip': 0,
+      'top': 0,
+      'compareWithAppLocalTime': false
+    },
+  };
+
+  axios({
+    method: 'POST',
+    url: reqUrl,
+    headers,
+    data: body
+  })
+    .then(({ data }) => res.send(data))
+    .catch((err) => console.log(err));
+});
+
+// projects, non-json
 app.get('/api/morse/projects', (req, res) => {
   const reqUrl = `${morseSteelProjects}?a=API_DoQuery&fmt=structured&includeRids=1&usertoken=${token}`;
 
@@ -36,8 +77,10 @@ app.get('/api/morse/projects', (req, res) => {
     .catch((err) => console.log(err));
 });
 
-// json project table request
+// projects, json
 app.get('/api/morse/json/projects', (req, res) => {
+  const reqUrl = `https://api.quickbase.com/v1/records/query`;
+
   const headers = {
     'QB-Realm-Hostname': `${hostName}`,
     'User-Agent': 'Joe',
@@ -57,15 +100,14 @@ app.get('/api/morse/json/projects', (req, res) => {
       {
         'fieldId': 20,
         'grouping': 'ASC'
-      }
+      },
     ],
     'options': {
       'skip': 0,
       'top': 0,
       'compareWithAppLocalTime': false
-    }
+    },
   };
-  const reqUrl = `https://api.quickbase.com/v1/records/query`;
 
   axios({
     method: 'POST',
