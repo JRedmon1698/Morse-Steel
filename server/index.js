98eb88b98c1e390611bb0e55728aced04d320500
Joe Redmon <joesredmon@gmail.com>
2020-12-17 10:34:16 -0800

    working on converting to mvc framework

--- server/index.js
+++ server/index.js
@@ -3,30 +3,32 @@ const app = express();
 const bodyParser = require("body-parser");
 const path = require("path");
 const axios = require("axios");
-const token = require('../quickbase.js');
+// const token = require('../quickbase.js');
 const twilio = require('../twilio/twilioServer.js');
-const { client, sourceNumber, destinationNumber } = require('../twilio/config.js');
+// const { client, sourceNumber, destinationNumber } = require('../twilio/config.js');
+const Quickbase = require('./controllers/quickbase.js');
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 const PORT = 3002;
 
-const morseSteelTeamMembers = 'https://morsesteel.quickbase.com/db/bqv7vttbx';
-const morseSteelProjects = 'https://morsesteel.quickbase.com/db/bqs3mx37c';
-const hostName = 'morsesteel.quickbase.com';
+// const morseSteelTeamMembers = 'https://morsesteel.quickbase.com/db/bqv7vttbx';
+// const morseSteelProjects = 'https://morsesteel.quickbase.com/db/bqs3mx37c';
+// const hostName = 'morsesteel.quickbase.com';
 
-const appId = 'bqs3mx358';
-const projectTableId = 'bqs3mx37c';
-const teamMemberTableId = 'bqv7vttbx';
+// const appId = 'bqs3mx358';
+// const projectTableId = 'bqs3mx37c';
+// const teamMemberTableId = 'bqv7vttbx';
 
 // team members, non-json
 app.get('/api/morse/team', (req, res) => {
-  const reqUrl = `${morseSteelTeamMembers}?a=API_DoQuery&fmt=structured&includeRids=1&query={10.EX.'Foreman'}&usertoken=${token}`;
+  // const reqUrl = `${morseSteelTeamMembers}?a=API_DoQuery&fmt=structured&includeRids=1&query={10.EX.'Foreman'}&usertoken=${token}`;
 
-  axios(reqUrl)
-    .then(({ data }) => res.send(data))
-    .catch((err) => console.log(err));
+  // axios(reqUrl)
+  //   .then(({ data }) => res.send(data))
+  //   .catch((err) => console.log(err));
+  Quickbase.getTeamInfo(req, res);
 });
 
 // team members, json
@@ -118,21 +120,44 @@ app.get('/api/morse/json/projects', (req, res) => {
     headers,
     data: body
   })
-    .then(({ data }) => res.send(data))
+    // .then(({ data }) => res.send(data))
+    .then(({ data }) => {
+      let message = '';
+      for (let i = 0; i < data.data.length; i += 1) {
+        if (data.data[i][20].value === '4862') {
+          message = (`Hello, ${data.data[0][113].value}, you should report for 
+          the ${data.data[0][132].value} project on ${data.data[0][30].value}.`)
+        }
+      }
+      console.log(message);
+      // .then((data) => {
+        console.log(message);
+        client.messages
+        .create({
+          body: message,
+          from: `${sourceNumber}`,
+          to: `${destinationNumber}`
+        })
+      })
+    // })
     .catch((err) => console.log(err));
 });
 
 // twilio test call
 app.post('/api/twilio/test', (req, res) => {
+
+  axios('/api/morse/json/projects')
+    .then((data) => res.send(data))
+    .catch((err) => console.log(err));
   
-  client.messages
-  .create({
-     body: 'This is a test.',
-     from: `${sourceNumber}`,
-     to: `${destinationNumber}`
-   })
-  .then(message => res.send('Success'))
-  .catch((err) => console.log(err));
+  // client.messages
+  // .create({
+  //    body: 'This is a test.',
+  //    from: `${sourceNumber}`,
+  //    to: `${destinationNumber}`
+  //  })
+  // .then(message => res.send('Success'))
+  // .catch((err) => console.log(err));
 
 });
 
