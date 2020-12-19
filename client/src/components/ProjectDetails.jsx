bd702a8ee24d578b70cbaeca35fe4b644807b47e
Joe Redmon <joesredmon@gmail.com>
2020-12-19 11:26:43 -0800

    added add team member button to proj deets

--- client/src/components/ProjectDetails.jsx
+++ client/src/components/ProjectDetails.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ProjectDetails = ({ projectDetails, setProjectDetails }) => {
-  
+
   const getDetail = (value) => {
     if (value === '' || value === null) {
       return 'none on file';
@@ -14,6 +14,16 @@ const ProjectDetails = ({ projectDetails, setProjectDetails }) => {
     <div>
       <p>Project Name: {getDetail(projectDetails[21].value)}</p>
       <p>Project Number: {getDetail(projectDetails[21].value)}</p>
+      <p>Project Status: {getDetail(projectDetails[22].value)}</p>
+      <p>Team Members: {getDetail(projectDetails[113].value)}</p>
+      <p>Est. Start Date: {getDetail(projectDetails[27].value)}</p>
+      <p>Est. End Date: {getDetail(projectDetails[30].value)}</p>
+      <p>Contracctor Name: {getDetail(projectDetails[129].value)}</p>
+      <p>PO #: {getDetail(projectDetails[80].value)}</p>
+      <p>Tasks: {getDetail(projectDetails[120].value)}</p>
+      <div>
+        <AddTeamMemberButton>Assign Team Members</AddTeamMemberButton>
+      </div>
       <BackButton onClick={() => setProjectDetails(null)}>Back</BackButton>
     </div>
   );
@@ -27,4 +37,17 @@ const BackButton = styled.button`
   }
   margin-top: 15px;
   margin-bottom: 40px;
+`;
+
+const AddTeamMemberButton = styled.button`
+  border-radius: 4px;
+  padding: 6px;
+  font-weight: 450;
+  margin-bottom: 15px;
+  border: 2px solid green;
+  margin-left 20px;
+  :hover {
+    cursor: pointer;
+    box-shadow: inset 0 0 3px #000000;
+  }
 `;
