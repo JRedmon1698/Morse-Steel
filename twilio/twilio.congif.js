1f25a01fc4e82693849f5a1c6a1b012ea8b06c0f
Joe Redmon <62921426+JRedmon1698@users.noreply.github.com>
2021-01-14 16:43:50 -0800

    Update twilio.congif.js

--- twilio/twilio.congif.js
+++ twilio/twilio.congif.js
@@ -1,9 +1,3 @@
-const accountSid = 'ACb5238abd3767ce35a284e149ba2f5ed8';
-const authToken = '5e7f606b7c1abf44d8e6894720b402c9';
-const client = require('twilio')(accountSid, authToken);
-
-const sourceNumber = '+12516071813';
-const destinationNumber = '+19073015054';
 
 // *** EXAMPLE MSG FORMAT ***
 
